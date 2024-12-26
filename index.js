b774497ead40cbad1f325fa9dcc49b01f9d906cb
nick the knife <nickalons@outlook.com>
2024-12-26 17:51:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const puppeteer = require('puppeteer');
 
 (async() => {
-const browser = await puppeteer.launch();
-console.log("Bro Is Amazing")
+const browser = await puppeteer.launch({
+         headless: "new",
+        args: ['--no-sandbox']
+});
+  console.log("Bro Is Amazing")
 const page = await browser.newPage();
 await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36");
 await page.setViewport({ width: 1280, height: 720 });
